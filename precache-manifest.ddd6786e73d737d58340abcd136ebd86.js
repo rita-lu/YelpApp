e2bacaa5bc49dc3a20f491160fd2465478bd836f
Rita Lu <ritalu@Ritas-MacBook-Pro.local>
2022-10-29 19:09:03 -0700

    Updates

---
+++ precache-manifest.ddd6786e73d737d58340abcd136ebd86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06de69e79e3585e0534196eb426ee44e",
+    "url": "/YelpApp/index.html"
+  },
+  {
+    "revision": "f7a63bc9c0a0fbbd9e36",
+    "url": "/YelpApp/static/css/main.d4e3903d.chunk.css"
+  },
+  {
+    "revision": "1407e268aa838c91711d",
+    "url": "/YelpApp/static/js/2.67de0bb3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/YelpApp/static/js/2.67de0bb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7a63bc9c0a0fbbd9e36",
+    "url": "/YelpApp/static/js/main.a2c10305.chunk.js"
+  },
+  {
+    "revision": "535fbd8f59745724573f",
+    "url": "/YelpApp/static/js/runtime-main.8b754ff9.js"
+  }
+]);
